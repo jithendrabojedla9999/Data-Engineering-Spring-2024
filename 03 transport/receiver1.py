8e543c36b5138288998458a69ec5be6b058732f1
Jithendra Bojedla <79437513+jithendrabojedla9999@users.noreply.github.com>
2024-04-19 12:51:05 -0700

    Add files via upload

---
+++ 03 transport/receiver1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 12:40:23 2024
+
+@author: Jithendra
+"""
+
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+
+project_id = "pubsubtask16042024"
+subscription_id = "MySub"
+timeout = 5.0
+
+# Initialize the Pub/Sub Subscriber client
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+# Define the callback function to process incoming messages
+def callback(message):
+    print(f"Received message: {message.data.decode('utf-8')}")
+    message.ack()
+
+# Subscribe to the Pub/Sub topic
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}...\n")
+
+# Wait for messages
+try:
+    streaming_pull_future.result(timeout=timeout)
+except TimeoutError:
+    streaming_pull_future.cancel()
+    streaming_pull_future.result()  
---
+++ 03 transport/updated_publisher1.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 12:39:25 2024
+
+@author: Jithendra
+"""
+
+import requests
+import json
+import time
+from google.cloud import pubsub_v1
+
+# Set your Google Cloud project ID and Pub/Sub topic ID
+project_id = "pubsubtask16042024"
+topic_id = "MyTopic"
+
+# Initialize the Pub/Sub Publisher client
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+
+# Function to fetch data from the API and publish to Pub/Sub
+def fetch_and_publish_data(vehicle_id):
+    url = f"https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id={vehicle_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        with open(f"bcsample_{vehicle_id}.json", "w") as file:
+            json.dump(data, file)
+        for record in data:
+            data_str = json.dumps(record)
+            data_bytes = data_str.encode("utf-8")
+            future = publisher.publish(topic_path, data=data_bytes)
+            print(f"Published message: {future.result()}")
+            time.sleep(0.25)
+    else:
+        print(f"Failed to fetch data for vehicle ID {vehicle_id}")
+
+vehicle_ids = ["3010", "3951"]
+
+for vehicle_id in vehicle_ids:
+    fetch_and_publish_data(vehicle_id)
+
+print(f"Published messages to {topic_path}.")
